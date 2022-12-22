d89fbbf4ac96cba8678217f56349f0b4b02ed655
Hazem Kawas <hazem.k.4488@gmail.com>
2022-12-22 15:56:43 +0100

    Co-authored-by: Erik Isaksson <erik.isaksson@medieinstitutet.se> Co-authored-by: Philip Risberg <Prisberg@users.noreply.github.com>
    Set yup the project structure

--- src/components/__tests__/Button.test.tsx
+++ src/components/__tests__/Button.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 
-import { Button } from '../buttons/Button';
+import { Button } from '../Buttons/Button';
 
 it('functions as a button', () => {
   const onPress = jest.fn();
