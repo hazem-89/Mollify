4bb926403299b2e6c2b100c8dd229f4bd63da958
Hazem Kawas <hazem.k.4488@gmail.com>
2022-12-22 16:05:41 +0100

    update folders names

--- src/components/__tests__/Button.test.tsx
+++ src/components/__tests__/Button.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 
-import { Button } from '../Buttons/Button';
+import { Button } from '../buttons/Button';
 
 it('functions as a button', () => {
   const onPress = jest.fn();
