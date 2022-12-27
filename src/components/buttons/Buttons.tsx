698a4907331b231b9715bc48fc3da2b59f9629ed
Hazem Kawas <hazem.k.4488@gmail.com>
2022-12-27 09:55:51 +0100

    add cancel button

--- src/components/buttons/Buttons.tsx
+++ src/components/buttons/Buttons.tsx
@@ -1,24 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import {
-  View,
   StyleSheet,
-  Image,
   ImageBackground,
-  Alert,
   TouchableOpacity,
   ImageSourcePropType,
-  StyleProp,
 } from 'react-native';
 import { Text } from '../Text';
-import colors from '../../constants/colors';
 import { useDimensions } from '@react-native-community/hooks';
 import GreenButtonImage from '../../../assets/Images/GreenButton.png';
 import GoldenButton from '../../../assets/Images/GoldenButton.png';
-import GoogleButton from '../../../assets/Images/GoogleButton.png';
+import GoogleButton from '../../../assets/Images/GoogleButtonWithIcon.png';
+import GoogleButtonBroken from '../../../assets/Images/GoogleButtonBroken.png';
+import CancelButton from '../../../assets/Images/CancelButton.png';
 
 type ButtonProps = {
-  onPress: () => void;
-  text: string;
+  onPress?: () => void;
+  text?: any;
   background: string;
   type: string;
 };
@@ -35,22 +32,27 @@ function Button({
     undefined,
   );
   const [style, setStyle] = useState<Object>({});
-  const handleMenu = () => {
-    // signUpMenuOpen ? setSignUpMenuOpen(false) : setSignUpMenuOpen(true);
-  };
+
   const styles = StyleSheet.create({
     Green: {
       justifyContent: 'center',
       width: smallScreen ? 200 : 250,
-      height: smallScreen ? 40 : 50,
+      height: smallScreen ? 50 : 65,
       alignSelf: 'center',
+    },
+    GreenForms: {
+      justifyContent: 'center',
+      width: smallScreen ? 120 : 150,
+      height: smallScreen ? 25 : 35,
+      alignSelf: 'center',
+      marginTop: 30,
     },
     Google: {
       justifyContent: 'center',
-      width: smallScreen ? 200 : 300,
-      height: smallScreen ? 40 : 70,
+      width: smallScreen ? 220 : 300,
+      height: smallScreen ? 50 : 70,
       alignSelf: 'center',
-      marginTop: 20,
+      marginTop: smallScreen ? 10 : 20,
     },
     Gold: {
       justifyContent: 'center',
@@ -58,6 +60,14 @@ function Button({
       height: smallScreen ? 45 : 55,
       alignSelf: 'center',
       resizeMode: 'contain',
+    },
+    CancelButton: {
+      position: 'absolute',
+      right: smallScreen ? 30 : 35,
+      top: smallScreen ? 30 : 40,
+      width: smallScreen ? 35 : 55,
+      height: smallScreen ? 35 : 55,
+      zIndex: 100,
     },
   });
 
@@ -70,6 +80,15 @@ function Button({
       setStyle(styles.Green);
     } else if (background === 'Google') {
       setSource(GoogleButton);
+      setStyle(styles.Google);
+    } else if (background === 'CancelButton') {
+      setSource(CancelButton);
+      setStyle(styles.CancelButton);
+    } else if (background === 'GreenForms') {
+      setSource(GreenButtonImage);
+      setStyle(styles.GreenForms);
+    } else if (background === 'GoogleButtonBroken') {
+      setSource(GoogleButtonBroken);
       setStyle(styles.Google);
     }
   }, [background]);
