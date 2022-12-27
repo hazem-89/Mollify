ca90b1dba0556128e99ca7c90938446b822f3197
Hazem Kawas <hazem.k.4488@gmail.com>
2022-12-27 09:58:07 +0100

    add sound and broken google button for test

--- src/screens/StartPage.tsx
+++ src/screens/StartPage.tsx
@@ -11,24 +11,51 @@ import MainBackGround from '../../assets/Images/MainBackGround.png';
 import WelcomeSign from '../../assets/Images/WelcomeSign.png';
 import Tiger from '../../assets/Images/tiger-min.png';
 import { useDimensions } from '@react-native-community/hooks';
-import { SignUp } from '../components/forms/SignUp';
-import StartPageButtons from '../components/buttons/Buttons';
+import { SignUp } from '../components/forms/Signup';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { MainStackParams } from '../navigation/Main';
-import { useNavigation } from '@react-navigation/native';
 import Button from '../components/buttons/Buttons';
 import { Text } from '../components/Text';
+import { Audio } from 'expo-av';
 
 type Props = {
   navigation: StackNavigationProp<MainStackParams, 'StartPage'>;
 };
 export const StartPage: React.FC<Props> = ({ navigation }: Props) => {
   const dimensions = useDimensions();
-
   const [smallScreen] = useState(dimensions.screen.height < 600 ? true : false);
   const [signUpMenuOpen, setSignUpMenuOpen] = useState(false);
+  const [googleBtnPressed, setGoogleBtnPressed] = useState(false);
   const handleMenu = () => {
     signUpMenuOpen ? setSignUpMenuOpen(false) : setSignUpMenuOpen(true);
+  };
+
+  const [sound, setSound] = React.useState<any>();
+
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync(
+      require('../../assets/sounds/ice.mp3'),
+    );
+    setSound(sound);
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  const handleGoogleBtnClick = () => {
+    setGoogleBtnPressed(true);
+    playSound();
+  };
+  const handleGoogleBtnClick1 = () => {
+    setGoogleBtnPressed(false);
   };
   const styles = StyleSheet.create({
     container: {
@@ -40,23 +67,21 @@ export const StartPage: React.FC<Props> = ({ navigation }: Props) => {
     WelcomeSign: {
       justifyContent: 'center',
       alignSelf: 'center',
-      width: smallScreen ? 250 : 450,
-      height: smallScreen ? 75 : 150,
+      width: smallScreen ? 350 : 450,
+      height: smallScreen ? 100 : 150,
       marginTop: 30,
+      zIndex: 10,
     },
     backGroundImage: {
       width: '100%',
       height: '100%',
     },
     menuContainer: {
-      // width: 500,
-      // height: 400,
       position: 'absolute',
       alignSelf: 'center',
       justifyContent: 'center',
-      top: '10%',
+      top: '25%',
       flex: 1,
-      backgroundColor: 'tomato',
     },
     tiger: {
       position: 'absolute',
@@ -66,40 +91,63 @@ export const StartPage: React.FC<Props> = ({ navigation }: Props) => {
       height: 180,
       width: 130,
     },
+    CancelButton: {
+      position: 'absolute',
+    },
+    GoogleIcon: {
+      position: 'absolute',
+      width: 50,
+      height: 50,
+    },
   });
 
   return (
     <SafeAreaView style={styles.container}>
       <ImageBackground source={MainBackGround} style={styles.backGroundImage}>
-        <View>
-          <Image source={WelcomeSign} style={styles.WelcomeSign} />
-          <Button
-            background="Gold"
-            text="Click me"
-            onPress={handleMenu}
-            type="Gold"
-          />
-          <Button
-            background="Google"
-            text="sign in with Google"
-            onPress={handleMenu}
-            type="Google"
-          />
-          <Text type="bold">OR</Text>
-          <Button
-            background="Green"
-            text="Create account"
-            onPress={handleMenu}
-            type="Green"
-          />
-        </View>
+        <Image source={WelcomeSign} style={styles.WelcomeSign} />
+        {!signUpMenuOpen ? (
+          <View>
+            <Button
+              background="Gold"
+              text="Sign in"
+              onPress={handleMenu}
+              type="Gold"
+            />
+            {!googleBtnPressed ? (
+              <Button
+                background="Google"
+                text={'sign in with Google'}
+                onPress={handleGoogleBtnClick}
+                type="Google"
+              />
+            ) : (
+              <Button
+                background="GoogleButtonBroken"
+                onPress={handleGoogleBtnClick1}
+                type="Google"
+              />
+            )}
+
+            <Text type="bold">OR</Text>
+            <Button
+              background="Green"
+              text="Create account"
+              onPress={handleMenu}
+              type="Green"
+            />
+          </View>
+        ) : null}
+
         {signUpMenuOpen ? (
           <View style={styles.menuContainer}>
-            <SignUp />
+            <SignUp
+              signUpMenuOpen={signUpMenuOpen}
+              setSignUpMenuOpen={setSignUpMenuOpen}
+            />
           </View>
         ) : null}
       </ImageBackground>
-      <Image source={Tiger} style={styles.tiger} />
+      {signUpMenuOpen ? null : <Image source={Tiger} style={styles.tiger} />}
     </SafeAreaView>
   );
 };
