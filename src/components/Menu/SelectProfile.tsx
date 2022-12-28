1a17bf5548ee28b8173be5fa2c8d937f53a06836
Hazem Kawas <hazem.k.4488@gmail.com>
2022-12-28 21:34:57 +0100

    fix error

---
+++ src/components/Menu/SelectProfile.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const SelectProfile = () => {
+  return (
+    <View>
+      <Text>SelectProfile</Text>
+    </View>
+  );
+};
+
+export default SelectProfile;
+
+const styles = StyleSheet.create({});
