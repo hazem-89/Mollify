762c7eda3aac6b0e617e8b272a084ee1cc7f32d9
Hazem Kawas <hazem.k.4488@gmail.com>
2023-01-09 16:07:46 +0100

    update tasks interface

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -1,10 +1,11 @@
 export interface Tasks {
-  id?: string;
-  taskTitle?: string;
-  taskDescription?: string;
-  pointsValue?: string;
+  id: string;
+  taskTitle: string;
+  taskDescription: string;
+  pointsValue: string;
   endTime: string;
-  category?: string;
+  category: string;
   isDone: boolean;
   hasRequest: boolean;
+  profileId: string;
 }
