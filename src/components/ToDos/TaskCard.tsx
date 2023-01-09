4c5a83e847752d40a8734b07a2896269046586ee
Hazem Kawas <hazem.k.4488@gmail.com>
2023-01-09 10:36:51 +0100

    add three buttons, delete, edit and mark as done on swipe left or right

--- src/components/ToDos/TaskCard.tsx
+++ src/components/ToDos/TaskCard.tsx
@@ -4,6 +4,7 @@ import {
   StyleSheet,
   TouchableOpacity,
   View,
+  Animated,
 } from 'react-native';
 import { Tasks } from '../../Interfaces';
 import PointsBackground from '../../../assets/Images/Icons/PointsBackground.png';
@@ -12,7 +13,8 @@ import { useDimensions } from '@react-native-community/hooks';
 import colors from '../../constants/colors';
 import { Text } from '../../components/Text';
 import { CountdownTimer } from './CountDown';
-
+import { Swipeable } from 'react-native-gesture-handler';
+import Button from '../buttons/Buttons';
 interface Props {
   task: Tasks;
 }
@@ -28,12 +30,10 @@ const TaskCard = ({ task }: Props) => {
     CardContainer: {
       flex: 1,
       flexDirection: 'row',
-      justifyContent: 'space-between',
       marginTop: 10,
-      borderBottomWidth: 1,
-      borderBottomColor: colors.primary,
+
       maxHeight: smallScreen ? 50 : 75,
-      maxWidth: smallScreen ? 350 : 500,
+      alignItems: 'center',
       marginLeft: smallScreen ? 30 : 50,
     },
     TextView: {
@@ -51,10 +51,53 @@ const TaskCard = ({ task }: Props) => {
       height: smallScreen ? 40 : 60,
     },
   });
+  const leftSwipe = (
+    progress: any,
+    dragX: {
+      interpolate: (arg0: {
+        inputRange: number[];
+        outputRange: number[];
+        extrapolate: string;
+      }) => any;
+    },
+  ) => {
+    const scale = dragX.interpolate({
+      inputRange: [0, 100],
+      outputRange: [0, 1],
+      extrapolate: 'clamp',
+    });
+    return (
+      <View style={{ flexDirection: 'row', minWidth: 100 }}>
+        <Button background="DeleteTask" onPress={() => console.warn(task.id)} />
+        <TouchableOpacity
+          style={{
+            width: smallScreen ? 30 : 50,
+            height: smallScreen ? 30 : 50,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Animated.Text style={{ transform: [{ scale: scale }] }}>
+            Edit
+          </Animated.Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+  const leftSwipeChilde = () => {
+    return null;
+  };
+  const rightSwipe = () => {
+    return (
+      <View>
+        <Button background="DoneIcon" onPress={() => console.warn(task.id)} />
+      </View>
+    );
+  };
 
   return (
     <View style={styles.CardContainer}>
-      {parent && (
+      {/* {parent && (
         <View
           style={{
             position: 'absolute',
@@ -66,44 +109,61 @@ const TaskCard = ({ task }: Props) => {
             justifyContent: 'center',
             borderRadius: 50,
           }}
+        ></View>
+      )} */}
+      <TouchableOpacity activeOpacity={0.6}>
+        <Swipeable
+          renderLeftActions={parent ? leftSwipe : leftSwipeChilde}
+          renderRightActions={rightSwipe}
         >
-          <TouchableOpacity>
-            <Text>E</Text>
-          </TouchableOpacity>
-        </View>
-      )}
-      <View style={styles.TextView}>
-        <Text type="todoList">{task.taskDescription}</Text>
-        {/* <Text type="todoList">{date}</Text> */}
-      </View>
-      <ImageBackground
-        source={PointsBackground}
-        style={styles.PointsBackground}
-      >
-        <View
-          style={{
-            width: smallScreen ? 40 : 60,
-            height: smallScreen ? 40 : 60,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Text type="todoList">{task.pointsValue}</Text>
-        </View>
-      </ImageBackground>
-      <ImageBackground source={TimeBackground} style={styles.TimeBackground}>
-        <View
-          style={{
-            width: smallScreen ? 75 : 100,
-            height: smallScreen ? 40 : 50,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <CountdownTimer date={endDate} />
-          {/* <Text type="todoList">{task.endTime}</Text> */}
-        </View>
-      </ImageBackground>
+          <View
+            style={{
+              flexDirection: 'row',
+              width: smallScreen ? 350 : 500,
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderBottomWidth: 1,
+              borderBottomColor: colors.primary,
+            }}
+          >
+            <View style={styles.TextView}>
+              <Text type="todoList">{task.taskDescription}</Text>
+              {/* <Text type="todoList">{date}</Text> */}
+            </View>
+            <ImageBackground
+              source={PointsBackground}
+              style={styles.PointsBackground}
+            >
+              <View
+                style={{
+                  width: smallScreen ? 40 : 60,
+                  height: smallScreen ? 40 : 60,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <Text type="todoList">{task.pointsValue}</Text>
+              </View>
+            </ImageBackground>
+            <ImageBackground
+              source={TimeBackground}
+              style={styles.TimeBackground}
+            >
+              <View
+                style={{
+                  width: smallScreen ? 75 : 100,
+                  height: smallScreen ? 40 : 50,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <CountdownTimer date={endDate} />
+                {/* <Text type="todoList">{task.endTime}</Text> */}
+              </View>
+            </ImageBackground>
+          </View>
+        </Swipeable>
+      </TouchableOpacity>
     </View>
   );
 };
