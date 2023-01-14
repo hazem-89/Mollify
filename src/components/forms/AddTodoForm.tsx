268c37c5f049d9873276a2415564a0350be49de4
Hazem Kawas <hazem.k.4488@gmail.com>
2023-01-14 20:43:27 +0100

    fix add cleaning task style

--- src/components/forms/AddTodoForm.tsx
+++ src/components/forms/AddTodoForm.tsx
@@ -1,4 +1,11 @@
-import { Alert, StyleSheet, TouchableOpacity, View, Image } from 'react-native';
+import {
+  Alert,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Image,
+  ImageBackground,
+} from 'react-native';
 import React, { Dispatch, SetStateAction, useState } from 'react';
 import { useDimensions } from '@react-native-community/hooks';
 import uuid from 'react-native-uuid';
@@ -10,6 +17,10 @@ import laundryBasket from '../../../assets/images/Icons/basket.png';
 import { Text } from '../../components/Text';
 import hourglass from '../../../assets/images/Icons/hourglass.png';
 import PointsIcon from '../../../assets/images/Icons/PointsIcon.png';
+import TaskTextBg from '../../../assets/images/TaskTextBg.png';
+import CleaningTasksBg from '../../../assets/images/CleaningTasksBg.png';
+import ActiveCleaningTasksBg from '../../../assets/images/ActiveCleaningTasksBg.png';
+
 import { useDataContext } from '../../util/context/DataContext';
 
 const CleaningTodo = [
@@ -48,9 +59,15 @@ type ErrorType = {
 
 type todoFormProps = {
   category: string;
+  setAddTaskBtnClicked: React.Dispatch<
+    React.SetStateAction<string | undefined>
+  >;
 };
 
-export const AddTodoForm = ({ category }: todoFormProps) => {
+export const AddTodoForm = ({
+  category,
+  setAddTaskBtnClicked,
+}: todoFormProps) => {
   const [errors, setErrors]: [ErrorType, Dispatch<SetStateAction<{}>>] =
     React.useState({});
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
@@ -64,13 +81,17 @@ export const AddTodoForm = ({ category }: todoFormProps) => {
     selected: '',
   });
   const { addDocToFS } = useDataContext();
-
+  const test = () => {
+    setAddTaskBtnClicked(undefined);
+  };
   const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      minHeight: smallScreen ? 380 : 400,
-      maxWidth: smallScreen ? 550 : 300,
-      padding: smallScreen ? 40 : 50,
+      // maxWidth: smallScreen ? 580 : 700,
+      minHeight: smallScreen ? 300 : 500,
+      flexDirection: 'row',
+      // alignItems: 'flex-end',
+      // alignItems: 'center',
+      justifyContent: 'center',
     },
     input: {
       justifyContent: 'center',
@@ -81,43 +102,53 @@ export const AddTodoForm = ({ category }: todoFormProps) => {
       width: smallScreen ? 250 : 350,
       alignItems: 'flex-start',
     },
+    PointsInput: {
+      width: smallScreen ? 80 : 100,
+    },
     inputContainer: {
       alignItems: 'center',
       marginTop: 40,
     },
     TimePointsContainer: {
       flexDirection: 'row',
-      alignItems: 'center',
       justifyContent: 'center',
-      marginVertical: 20,
+      marginVertical: smallScreen ? 0 : 20,
+      // maxWidth: 300,
+      // width: smallScreen ? 580 : 700,
     },
     TimePointView: {
       width: smallScreen ? 150 : 200,
       alignItems: 'center',
       justifyContent: 'center',
     },
-    AvatarContainer: {
-      width: smallScreen ? 50 : 100,
-      height: smallScreen ? 50 : 100,
+    TasksContainer: {
+      width: smallScreen ? 120 : 140,
+      height: smallScreen ? 95 : 110,
       alignItems: 'center',
       justifyContent: 'center',
-      backgroundColor: '#FF7A00',
+      // backgroundColor: '#FF7A00',
       borderRadius: 500,
-      marginLeft: 5,
-    },
-    AvatarContainerSelected: {
-      width: smallScreen ? 50 : 100,
-      height: smallScreen ? 50 : 100,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: 'green',
-      marginRight: 10,
-      borderRadius: 500,
+      marginLeft: 20,
+      marginBottom: 10,
     },
     icons: {
       width: smallScreen ? 25 : 40,
       height: smallScreen ? 40 : 70,
       marginRight: 10,
+    },
+    CleaningTasksInfo: {
+      // maxHeight: 100,
+      maxWidth: smallScreen ? 150 : 200,
+      position: 'absolute',
+      left: smallScreen ? -270 : -280,
+      top: smallScreen ? 110 : 140,
+    },
+    OtherTasksInfo: {
+      // maxHeight: 100,
+      maxWidth: smallScreen ? 150 : 200,
+      position: 'absolute',
+      left: smallScreen ? -300 : -350,
+      top: smallScreen ? 60 : 140,
     },
   });
 
@@ -200,141 +231,157 @@ export const AddTodoForm = ({ category }: todoFormProps) => {
   }
 
   return (
-    <View>
-      {category === 'Room' && (
-        <>
-          <View
-            style={{
-              flexDirection: 'row',
-              width: smallScreen ? 300 : 500,
-              alignItems: 'center',
-              justifyContent: 'center',
-              marginTop: smallScreen ? 20 : 30,
-            }}
-          >
-            {CleaningTodo?.map(todo => {
-              return (
-                <TouchableOpacity
-                  key={todo.title}
-                  onPress={() => handlePress(todo)}
-                >
+    <View style={styles.container}>
+      <View style={{ alignItems: 'center' }}>
+        {category === 'Room' && (
+          <>
+            <View style={styles.CleaningTasksInfo}>
+              <Text type="MenuTitle">
+                Here you can Add a room cleaning task
+              </Text>
+              {/* <Text type="MenuTitle">
+                Adding a task will automatically add an item to the room
+              </Text> */}
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                width: smallScreen ? 300 : 500,
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginTop: smallScreen ? 20 : 30,
+              }}
+            >
+              {CleaningTodo?.map(todo => {
+                return (
                   <View
-                    style={
-                      todo.title !== state.selected
-                        ? styles.AvatarContainer
-                        : styles.AvatarContainerSelected
-                    }
+                    key={todo.title}
+                    style={{
+                      minWidth: smallScreen ? 100 : 120,
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}
                   >
-                    <Image
-                      source={todo.img}
-                      style={{
-                        width: smallScreen ? 50 : 50,
-                        resizeMode: 'stretch',
-                      }}
-                    />
+                    <TouchableOpacity onPress={() => handlePress(todo)}>
+                      <ImageBackground
+                        style={styles.TasksContainer}
+                        source={
+                          todo.title !== state.selected
+                            ? CleaningTasksBg
+                            : ActiveCleaningTasksBg
+                        }
+                      >
+                        <Image
+                          source={todo.img}
+                          style={{
+                            width: smallScreen ? 50 : 50,
+                            resizeMode: 'stretch',
+                          }}
+                        />
+                        <Text type="text">{todo.title}</Text>
+                      </ImageBackground>
+                    </TouchableOpacity>
                   </View>
-                </TouchableOpacity>
-              );
-            })}
-          </View>
-          <View style={{ alignItems: 'center' }}>
-            {state.taskTitle && (
-              <View
-                style={{
-                  marginVertical: 10,
-                  alignItems: 'flex-start', // marginLeft: 55,
-                  width: smallScreen ? 250 : 350,
-                }}
-              >
-                <Text type="text">{state.taskTitle}</Text>
-              </View>
-            )}
+                );
+              })}
+            </View>
             {state.taskDescription && (
-              <View
+              <ImageBackground
+                source={TaskTextBg}
                 style={{
-                  marginBottom: 10,
-                  alignItems: 'flex-start',
-                  width: smallScreen ? 250 : 350,
+                  marginVertical: smallScreen ? 10 : 20,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  width: smallScreen ? 300 : 400,
+                  height: smallScreen ? 60 : 80,
                 }}
               >
-                <Text type="todoList">Description:</Text>
-                <Text type="todoList">{state.taskDescription}</Text>
-              </View>
+                <Text type="text">{state.taskDescription}</Text>
+              </ImageBackground>
             )}
-          </View>
-        </>
-      )}
+          </>
+        )}
 
-      {category !== 'Room' && (
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="Task title:"
-            value={state.taskTitle}
-            onChangeText={(text: string) =>
-              setState({ ...state, taskTitle: text })
-            }
-            errorText={errors.taskTitle}
-            keyboardType="default"
-            autoCapitalize="none"
-            impStyle={styles.input}
-          />
-          <TextInput
-            placeholder="Task description:"
-            value={state.taskDescription}
-            onChangeText={(text: string) =>
-              setState({ ...state, taskDescription: text })
-            }
-            errorText={errors.taskDescription}
-            keyboardType="default"
-            autoCapitalize="none"
-            multiline={true}
-            impStyle={styles.input}
-          />
-        </View>
-      )}
-      <View style={styles.TimePointsContainer}>
-        <View style={styles.TimePointView}>
-          <TouchableOpacity
-            disabled={!state.taskTitle && !state.taskDescription}
-            onPress={showDatePicker}
-            style={{ flexDirection: 'row', alignItems: 'center' }}
-          >
-            <Image source={hourglass} style={styles.icons} />
-            <Text type="text">Set Time</Text>
-          </TouchableOpacity>
-          <DateTimePickerModal
-            style={{
-              shadowColor: '#fff',
-              shadowRadius: 0,
-              shadowOpacity: 1,
-              shadowOffset: { height: 0, width: 0 },
-            }}
-            isVisible={isDatePickerVisible}
-            mode="datetime"
-            onConfirm={handleConfirm}
-            onCancel={hideDatePicker}
-            minimumDate={new Date()}
-            minuteInterval={10}
-          />
-          <Text type="errorText">{errors.time}</Text>
-        </View>
-
-        <View style={styles.TimePointView}>
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Image source={PointsIcon} style={styles.icons} />
+        {category !== 'Room' && (
+          <View style={styles.inputContainer}>
+            <View style={styles.OtherTasksInfo}>
+              <Text type="MenuTitle">Here you can Add a {category} task</Text>
+            </View>
             <TextInput
-              placeholder="Set task Points"
+              placeholder="Task title:"
+              value={state.taskTitle}
+              onChangeText={(text: string) =>
+                setState({ ...state, taskTitle: text })
+              }
+              errorText={errors.taskTitle}
+              keyboardType="default"
               autoCapitalize="none"
-              value={pointsValue}
-              keyboardType="number-pad"
-              onChangeText={changedPin => setPointsValue(changedPin)}
+              impStyle={styles.input}
+            />
+            <TextInput
+              placeholder="Task description:"
+              value={state.taskDescription}
+              onChangeText={(text: string) =>
+                setState({ ...state, taskDescription: text })
+              }
+              errorText={errors.taskDescription}
+              keyboardType="default"
+              autoCapitalize="none"
+              multiline={true}
+              impStyle={styles.input}
             />
           </View>
-          <Text type="errorText">{errors.points}</Text>
+        )}
+        <View style={styles.TimePointsContainer}>
+          <View style={styles.TimePointView}>
+            <TouchableOpacity
+              disabled={!state.taskTitle && !state.taskDescription}
+              onPress={showDatePicker}
+              style={{ flexDirection: 'row', alignItems: 'center' }}
+            >
+              <Image source={hourglass} style={styles.icons} />
+              <Text type={endTime ? 'DigitalNum' : 'text'}>
+                {endTime ? endTime.toISOString().slice(0, 10) : 'Select Time'}
+              </Text>
+            </TouchableOpacity>
+            <DateTimePickerModal
+              isVisible={isDatePickerVisible}
+              mode="datetime"
+              onConfirm={handleConfirm}
+              onCancel={hideDatePicker}
+              minimumDate={new Date()}
+              minuteInterval={10}
+            />
+
+            <Text type="errorText">{errors.time}</Text>
+          </View>
+
+          <View style={styles.TimePointView}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Image source={PointsIcon} style={styles.icons} />
+              <TextInput
+                placeholder="Set task Points"
+                autoCapitalize="none"
+                value={pointsValue}
+                keyboardType="number-pad"
+                onChangeText={changedPin => setPointsValue(changedPin)}
+                impStyle={styles.PointsInput}
+              />
+            </View>
+            <Text type="errorText">{errors.points}</Text>
+          </View>
         </View>
-      </View>
-      <View style={{ marginBottom: smallScreen ? 10 : 0 }}>
-        <Button background="GreenForms" text="Add" onPress={() => submit()} />
+        <View
+          style={{
+            flexDirection: 'row',
+            minWidth: smallScreen ? 350 : 450,
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <Button background="GreenForms" text="Add" onPress={() => submit()} />
+          <Button background="Cancel" text="Cancel" onPress={() => test()} />
+        </View>
       </View>
     </View>
   );
