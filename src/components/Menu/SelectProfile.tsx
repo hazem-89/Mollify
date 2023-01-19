9250e33799543aaf1cf525729a06bcd66730d4a0
Hazem Kawas <hazem.k.4488@gmail.com>
2023-01-19 15:05:25 +0100

    add button shown only when first login and parent view

--- src/components/Menu/SelectProfile.tsx
+++ src/components/Menu/SelectProfile.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { useDimensions } from '@react-native-community/hooks';
+import { useNavigation } from '@react-navigation/native';
 import { DocumentData } from 'firebase/firestore';
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import {
+  Dimensions,
   Image,
   ImageBackground,
   StyleSheet,
@@ -21,9 +23,19 @@ import FormModal from '../modals/FormModal';
 const SelectProfile = () => {
   const [component, setComponent] = useState<ReactElement | undefined>();
   const dimensions = useDimensions();
-  const [smallScreen] = useState(dimensions.screen.height < 600);
-  const { profiles, loggedInProfile, filteredProfiles, setSelectedChild, setTasks, setRewards, } = useDataContext();
+  const navigation = useNavigation();
 
+  const [smallScreen] = useState(dimensions.screen.height < 600);
+  const {
+    profiles,
+    loggedInProfile,
+    filteredProfiles,
+    setSelectedChild,
+    setTasks,
+    setRewards,
+  } = useDataContext();
+
+  const ScreenWidth = Dimensions.get('window').width;
   const styles = StyleSheet.create({
     modal: {
       position: 'absolute',
@@ -34,7 +46,7 @@ const SelectProfile = () => {
     MainView: {
       position: 'absolute',
       top: smallScreen ? '5%' : '5%',
-      width: '80%',
+      width: 0.8 * ScreenWidth,
       height: smallScreen ? 300 : 500,
       flex: 1,
       alignItems: 'center',
@@ -61,7 +73,15 @@ const SelectProfile = () => {
       borderRadius: 500,
     },
   });
-
+  const handelNav = () => {
+    // console.log(navigationValue);
+    // @ts-ignore
+    navigation.navigate('TasksCategoryPage', {
+      paramKey: {
+        content: 'CreateProfile',
+      },
+    });
+  };
   function handleClick(state: string | undefined, profile?: DocumentData) {
     // setBtnClicked(state);
     switch (state) {
@@ -99,64 +119,79 @@ const SelectProfile = () => {
               Welcome {loggedInProfile.name}, select profile to manage
             </Text>
           ) : (
-            <Text type="header">Select profile to View</Text>
+            <View style={{ maxWidth: 0.4 * ScreenWidth }}>
+              {!profiles ? (
+                <Text type="header">
+                  Welcome! Start by Adding a parent profile
+                </Text>
+              ) : (
+                <Text type="header">Select profile</Text>
+              )}
+            </View>
           )}
         </View>
         <View style={styles.MainView}>
-          <View
-            style={{
-              width: '10%',
-              marginRight: 20,
-              alignContent: 'center',
-              justifyContent: 'center',
-              marginBottom: 20,
-            }}
-          >
-            <Button
-              background="AddButtonImage"
-              onPress={() => handleClick('CreateProfile')}
-            />
-          </View>
+          {(loggedInProfile && loggedInProfile.parent) || !profiles ? (
+            <>
+              <View
+                style={{
+                  width: '10%',
+                  marginRight: 20,
+                  alignContent: 'center',
+                  justifyContent: 'center',
+                  marginBottom: 20,
+                }}
+              >
+                <Button
+                  background="AddButtonImage"
+                  onPress={() => handelNav()}
+                />
+              </View>
+            </>
+          ) : (
+            <></>
+          )}
+
           <View style={styles.ProfilesView}>
             {filteredProfiles
               ? filteredProfiles?.map((profile: DocumentData) => (
-                <TouchableOpacity
-                  key={profile?.id}
-                  onPress={() => handleClick('ManageProfile', profile)}
-                >
-                  <View style={styles.profile}>
-                    <View style={styles.Avatar}>
-                      <Image
-                        source={TigerAvatar}
-                        style={{
-                          width: smallScreen ? 50 : 75,
-                          height: smallScreen ? 50 : 75,
-                        }}
-                      />
+                  <TouchableOpacity
+                    key={profile?.id}
+                    onPress={() => handleClick('ManageProfile', profile)}
+                  >
+                    <View style={styles.profile}>
+                      <View style={styles.Avatar}>
+                        <Image
+                          source={TigerAvatar}
+                          style={{
+                            width: smallScreen ? 50 : 75,
+                            height: smallScreen ? 50 : 75,
+                          }}
+                        />
+                      </View>
+                      <Text type="text">{profile?.name.toUpperCase()}</Text>
                     </View>
-                    <Text type="text">{profile?.name}</Text>
-                  </View>
-                </TouchableOpacity>
-              ))
+                  </TouchableOpacity>
+                ))
               : profiles?.map((profile: DocumentData) => (
-                <TouchableOpacity
-                  key={profile?.id}
-                  onPress={() => handleClick('EnterPIN', profile)}
-                >
-                  <View style={styles.profile}>
-                    <View style={styles.Avatar}>
-                      <Image
-                        source={TigerAvatar}
-                        style={{
-                          width: smallScreen ? 50 : 75,
-                          height: smallScreen ? 50 : 75,
-                        }}
-                      />
+                  <TouchableOpacity
+                    key={profile?.id}
+                    onPress={() => handleClick('EnterPIN', profile)}
+                  >
+                    <View style={styles.profile}>
+                      <View style={styles.Avatar}>
+                        <Image
+                          source={TigerAvatar}
+                          style={{
+                            width: smallScreen ? 50 : 75,
+                            height: smallScreen ? 50 : 75,
+                          }}
+                        />
+                      </View>
+                      <Text type="text">{profile?.name.toUpperCase()}</Text>
                     </View>
-                    <Text type="text">{profile?.name}</Text>
-                  </View>
-                </TouchableOpacity>
-              ))}
+                  </TouchableOpacity>
+                ))}
           </View>
         </View>
       </ImageBackground>
